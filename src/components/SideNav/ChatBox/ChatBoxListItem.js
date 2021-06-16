49c93c253913bfc0fb248e547efe67cbef0e3668
gitgregoryfuntera <gregory.funtera@gmail.com>
2021-06-16 20:08:04 +0800

    -added truncate username in chatbox list

--- src/components/SideNav/ChatBox/ChatBoxListItem.js
+++ src/components/SideNav/ChatBox/ChatBoxListItem.js
@@ -1,22 +1,28 @@
 import React from "react";
 import { ListGroupItem } from "react-bootstrap";
 import ProfileAvatar from "../../Profile/ProfileAvatar";
-import avatar from '../../../assets/profile.png';
+import avatar from "../../../assets/profile.png";
 import styles from "./ChatBoxListItem.module.css";
 const ChatBoxListItem = () => {
   return (
     <>
       <ListGroupItem>
-        <div className={`${styles['list-item-container']}`}>
+        <div className={`${styles["list-item-container"]}`}>
           <div className="list-avatar">
-             <ProfileAvatar avatar={avatar}/>
+            <ProfileAvatar avatar={avatar} />
           </div>
           <div className="list-username">
-            <p>Hello World</p>
+            <p className="font-weight-bold">
+              Hello World{" "}
+              <span className={`${styles["username-container"]} font-weight-normal`}>
+                Hello
+              </span>{" "}
+              <span className={`${styles["date-container"]} font-weight-normal`}>
+                Dec. 21, 2021
+              </span>
+            </p>
           </div>
         </div>
-        
-     
       </ListGroupItem>
       <ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
       <ListGroupItem>Vestibulum at eros</ListGroupItem>
